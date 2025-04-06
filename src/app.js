71537ad9ca53eddb01326161cbb4df3384d5e705
Arjun <kingarjun74280@gmail.com>
2025-04-06 15:05:20 +0530

    learn about express js

--- src/app.js
+++ src/app.js
@@ -1,14 +1,27 @@
 const express = require("express")
 const app = express()
+app.get("/user/:name/:pwd",(req,res)=>{
+    console.log(req.params)
+    res.send({fname:"Arjun",
+        lname:"Kathayat"
+    })
+})
+
+app.get(/^\/ab+cd$/, (req, res) => {
+    res.send("ok");
+  });
+  
+
+app.delete("/user",(req,res)=>{
+    res.send("Delete data successfully")
+})
 app.use("/hello",(req,res)=>{
     res.send("Konichiwa")
 })
-app.use("/Thanku",(req,res)=>{
-    res.send("Arigato")
-})
-app.use((req,res)=>{
-    res.send("Welcome to Server")
-})
+
+
+
+
 app.listen(7000,()=>{
     console.log("Successfully  running on port 7000")
 })
